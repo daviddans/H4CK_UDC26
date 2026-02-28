9681d3b8da10b8201b3f07d22b2fe5e2610d9e68
loisbriones <lois.briones@udc.es>
2026-02-28 06:32:17 +0100

    Añadido pdfreader para procesar documentos

--- backend/src/indexer.py
+++ backend/src/indexer.py
@@ -1,17 +1,19 @@
 from opensearchpy import OpenSearch, helpers
 import sys
-from opensearch_dsl import Document, Text, Keyword
+from opensearch_dsl import Document, Text, Keyword, document
+from readpdf import limpiar_pdf
 
 
 class PdfDoc(Document):
-    authorName = Keyword()
     content = Text()
 
     def save(self, *args, **kwargs):
         return super(PdfDoc, self).save(*args, **kwargs)
 
 
-def index_document(client, index_name, document):
-    """Indexes a single document."""
-    response = client.index(index=index_name, body=document)
-    print(f"Indexed document ID: {response['_id']}")
+def index_document(client, index_name, path):
+    content = limpiar_pdf(path)
+    PdfDoc.init(using=client)
+
+    documento = PdfDoc(content=content)
+    documento.save(index=index_name)
